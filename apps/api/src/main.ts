ec285f3503534ba5e8d0c547cd589ce962bbb90e
Alessio Montel <montelalessio@gmail.com>
2025-11-03 16:27:57 +0100

    feat(api): remove default module and add standard packages in main

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,11 +1,37 @@
 import { NestFactory } from '@nestjs/core';
-
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import helmet from 'helmet';
+import cookieParser from 'cookie-parser';
+import pkg from '../package.json';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  await app.listen(3000);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
+  app.use(helmet());
+  app.enableCors({
+    // origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+    credentials: true,
+  });
+  app.use(cookieParser());
+
+  const config = new DocumentBuilder()
+    .setTitle('Participium API')
+    .setVersion(pkg.version)
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
+  app.setGlobalPrefix('api');
+
+  await app.listen(process.env.PORT ?? 5000);
 }
 
 void bootstrap();
