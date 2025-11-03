3388d30eaddc2874b22efa737914aa54ec75d68d
Alessio Montel <montelalessio@gmail.com>
2025-11-03 17:02:34 +0100

    feat(api): add repos to auth service

--- apps/api/src/modules/auth/auth.service.ts
+++ apps/api/src/modules/auth/auth.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { User } from '../../common/entities/user.entity';
+import { Account } from '../../common/entities/account.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+
+    @InjectRepository(Account)
+    private accountRepository: Repository<Account>,
+  ) {}
+}
