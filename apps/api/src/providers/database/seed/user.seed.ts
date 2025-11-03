9098497bbee288c6b35fe0bcd21a5ffefd0245b7
Alessio Montel <montelalessio@gmail.com>
2025-11-03 19:58:15 +0100

    feat(api): seed user

---
+++ apps/api/src/providers/database/seed/user.seed.ts
@@ -0,0 +1,44 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from '../../../app.module';
+import { DataSource } from 'typeorm';
+import { User, Account } from '@repo/api';
+import { nanoid } from 'nanoid';
+import bcrypt from 'bcrypt';
+import { faker, fi } from '@faker-js/faker';
+
+async function runSeed() {
+  const app = await NestFactory.createApplicationContext(AppModule);
+  const dataSource = app.get(DataSource);
+
+  // Seed user
+  const firstName = faker.person.firstName();
+  const lastName = faker.person.lastName();
+  const username = faker.internet.username({ firstName });
+  const email = `${firstName.toLowerCase()}@example.com`;
+  const password = 'password';
+
+  const user = await dataSource.getRepository(User).save({
+    id: nanoid(32),
+    firstName,
+    lastName,
+    username,
+    email,
+  });
+
+  // Seed account
+  await dataSource.getRepository(Account).save({
+    id: nanoid(32),
+    user,
+    providerId: 'local',
+    accountId: user.email,
+    password: await bcrypt.hash(password, 10),
+  });
+
+  console.log(`✅ Seeded user: ${email} / password: ${password}`);
+  await app.close();
+}
+
+runSeed().catch((err) => {
+  console.error('❌ Error seeding database', err);
+  process.exit(1);
+});
