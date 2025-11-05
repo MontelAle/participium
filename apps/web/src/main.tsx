36c2cf7a5f151bae18a1ff143d780bb8e0a8ec38
giova95 <angeligiovanni21@gmail.com>
2025-11-05 17:23:50 +0100

    fix(chore): migrate nextJS to react

---
+++ apps/web/src/main.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./style.css";
+
+const App = () => (
+  <div>
+    porcaccio dio
+  </div>
+);
+
+createRoot(document.getElementById("app")!).render(<App />);
