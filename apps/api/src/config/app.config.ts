9700a2d5a8cd219a69e944454ff323dea8b09163
Alessio Montel <montelalessio@gmail.com>
2025-11-09 14:39:06 +0100

    fix(api): use app.config correctly

--- apps/api/src/config/app.config.ts
+++ apps/api/src/config/app.config.ts
@@ -3,6 +3,7 @@ export default () => ({
     frontendUrl: process.env.FRONTEND_URL || 'localhost:5173',
     port: process.env.PORT || 5000,
     backendUrl: process.env.BACKEND_URL || 'http://localhost:5000/api',
+    env: process.env.NODE_ENV || 'development',
   },
   session: {
     expiresInSeconds: parseInt(
@@ -11,8 +12,16 @@ export default () => ({
     ),
   },
   cookie: {
-    httpOnly: process.env.COOKIE_HTTP_ONLY || true,
-    secure: process.env.COOKIE_SECURE || false,
+    httpOnly: process.env.COOKIE_HTTP_ONLY === 'true',
+    secure: process.env.COOKIE_SECURE === 'true',
     sameSite: process.env.COOKIE_SAME_SITE || 'lax',
+  },
+  db: {
+    type: process.env.DB_TYPE || 'postgres',
+    host: process.env.DB_HOST || 'localhost',
+    port: parseInt(process.env.DB_PORT || '5432', 10),
+    username: process.env.DB_USERNAME || 'admin',
+    password: process.env.DB_PASSWORD || 'password',
+    database: process.env.DB_DATABASE || 'participium',
   },
 });
