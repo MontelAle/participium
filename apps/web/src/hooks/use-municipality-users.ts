d166d41e9949e9bbf7e7684cd0395be923893dc1
ELEONORA PROCOPIO s344405 <s344405@studenti.polito.it>
2025-11-10 18:16:25 +0100

    feat : add update and delete in use-municiaplity-users.ts

--- apps/web/src/hooks/use-municipality-users.ts
+++ apps/web/src/hooks/use-municipality-users.ts
@@ -2,8 +2,13 @@ import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import {
   getMunicipalityUsers,
   createMunicipalityUser,
+  deleteMunicipalityUser,
+  updateMunicipalityUser,
+  getMunicipalityUser
 } from '../api/endpoints/municipality-users';
 import type { User } from '@repo/api';
+import { UpdateUserDto } from '@repo/api';
+
 
 export function useMunicipalityUsers() {
   return useQuery<User[]>({
@@ -16,6 +21,30 @@ export function useCreateMunicipalityUser() {
   const queryClient = useQueryClient();
   return useMutation({
     mutationFn: createMunicipalityUser,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['municipality-users'] });
+    },
+  });
+}
+
+export function useDeleteMunicipalityUser() {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: (id: string) => deleteMunicipalityUser(id),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['municipality-users'] });
+    },
+  });
+}
+
+
+export function useUpdateMunicipalityUser() {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: ({ userId, data }: { userId: string; data: UpdateUserDto }) =>
+      updateMunicipalityUser(userId, data),
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['municipality-users'] });
     },
