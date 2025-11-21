819ba8755511d6112aaf8021eafe04321ad297d0
ELEONORA PROCOPIO s344405 <s344405@studenti.polito.it>
2025-11-21 16:43:50 +0100

    feat : add useReport and useUpdateReport , add endpoints getReport and updateReport

--- apps/web/src/hooks/use-reports.ts
+++ apps/web/src/hooks/use-reports.ts
@@ -1,6 +1,9 @@
 import { useQuery, useQueryClient, useMutation } from '@tanstack/react-query';
 import type { Report } from '@repo/api';
 import { getReports, postReportWithImages } from '@/api/endpoints/reports';
+import { UpdateReportDto } from '@repo/api';
+import { updateReport } from '@/api/endpoints/reports';
+import { getReport } from '@/api/endpoints/reports';
 
 export function useReports() {
   return useQuery<Report[]>({
@@ -16,5 +19,31 @@ export function useCreateReport() {
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['reports'] });
     },
+  });
+}
+
+
+export function useUpdateReport() {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: ({
+      reportId,
+      data,
+    }: {
+      reportId: string;
+      data: UpdateReportDto;
+    }) => updateReport(reportId, data),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['reports'] });
+    },
+  });
+}
+
+export function useReport(reportId: string) {
+  return useQuery<Report>({
+    queryKey: ['report', reportId],
+    queryFn: () => getReport(reportId),
+    enabled: !!reportId, // evita la chiamata se reportId non è definito
   });
 }
