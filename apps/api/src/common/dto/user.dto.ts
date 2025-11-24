cf51e458ee1b74b4b14a9984d5778f01e0950529
mattialanza <mattialanza1@gmail.com>
2025-11-24 12:50:41 +0100

    feat(dto): add validation for telegram username in update profile DTO

--- apps/api/src/common/dto/user.dto.ts
+++ apps/api/src/common/dto/user.dto.ts
@@ -1,8 +1,19 @@
-import { IsOptional, IsString, IsBoolean } from 'class-validator';
+import {
+  IsOptional,
+  IsString,
+  IsBoolean,
+  Matches,
+  ValidateIf,
+} from 'class-validator';
 
 export class UpdateProfileDto {
   @IsString()
   @IsOptional()
+  @ValidateIf((o) => o.telegramUsername !== '')
+  @Matches(/^@[a-zA-Z0-9_]{4,31}$/, {
+    message:
+      'Telegram username must start with @ and contain 5-32 alphanumeric characters or underscores',
+  })
   telegramUsername?: string;
 
   @IsBoolean()
