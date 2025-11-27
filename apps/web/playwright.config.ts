4fa53cb1d31246540b7b40e81d44e6da35316e46
giova95 <angeligiovanni21@gmail.com>
2025-11-27 02:53:57 +0100

    feat: integrate Playwright for testing and update CI workflow

--- apps/web/playwright.config.ts
+++ apps/web/playwright.config.ts
@@ -21,12 +21,17 @@ export default defineConfig({
     },
 
     {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-    {
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
+    },
+
+    {
+      name: 'Mobile Chrome',
+      use: { ...devices['Pixel 5'] },
+    },
+    {
+      name: 'Mobile Safari',
+      use: { ...devices['iPhone 12'] },
     },
   ],
 
