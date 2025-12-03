789d195443d260b2fbe355474662c5b897c9f2d4
Alessio Montel <montelalessio@gmail.com>
2025-12-03 22:08:32 +0100

    refactor: remove jest package

--- apps/api/jest.config.ts
+++ apps/api/jest.config.ts
@@ -1,19 +1,32 @@
-import { nestConfig } from '@repo/jest-config';
 import type { Config } from 'jest';
 
+export const nestConfig = {
+    collectCoverage: true,
+    coverageProvider: 'v8',
+    moduleFileExtensions: ['js', 'ts', 'json'],
+    rootDir: 'src',
+    testRegex: '.*\\.spec\\.ts$',
+    transform: {
+        '^.+\\.(t|j)s$': 'ts-jest',
+    },
+    collectCoverageFrom: ['**/*.(t|j)s'],
+    coverageDirectory: '../coverage',
+    testEnvironment: 'node',
+} as const satisfies Config;
+
 const config: Config = {
-  ...nestConfig,
-  collectCoverageFrom: [
-    '<rootDir>/**/*.(t|j)s',
-    '!<rootDir>/**/*.spec.ts',
-    '!<rootDir>/**/*.module.ts',
-    '!<rootDir>/main.ts',
-    '!<rootDir>/**/seed/**/*.ts',
-    '!<rootDir>/**/*.type.ts',
-    '!<rootDir>/common/dto/**/*.ts',
-    '!<rootDir>/common/entities/**/*.ts',
-    '!<rootDir>/**/constants/error-messages.ts',
-  ],
+    ...nestConfig,
+    collectCoverageFrom: [
+        '<rootDir>/**/*.(t|j)s',
+        '!<rootDir>/**/*.spec.ts',
+        '!<rootDir>/**/*.module.ts',
+        '!<rootDir>/main.ts',
+        '!<rootDir>/**/seed/**/*.ts',
+        '!<rootDir>/**/*.type.ts',
+        '!<rootDir>/common/dto/**/*.ts',
+        '!<rootDir>/common/entities/**/*.ts',
+        '!<rootDir>/**/constants/error-messages.ts',
+    ],
 };
 
 export default config;
